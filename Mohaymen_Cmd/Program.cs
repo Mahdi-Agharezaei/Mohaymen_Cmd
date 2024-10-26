78c29fa2e4ddece375ed824ddc7364d93d6f9e53
Mahdi-Agharezaei <79786754+Mahdi-Agharezaei@users.noreply.github.com>
2024-10-26 21:06:46 +0330

    Add project files.

---
+++ Mohaymen_Cmd/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Mohaymen_Cmd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
