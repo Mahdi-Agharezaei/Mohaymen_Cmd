89f68755043f44780ddca9886f8df3e59fd05b47
Mahdi-Agharezaei <79786754+Mahdi-Agharezaei@users.noreply.github.com>
2024-10-26 23:08:53 +0330

    First commit (Add basic structure)

--- Mohaymen_Cmd/Program.cs
+++ Mohaymen_Cmd/Program.cs
@@ -4,7 +4,90 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> input = new List<string>();
+            bool IsLogin = false;
+            List<int> PartCounts = new List<int> { 1, 3, 5 };
+
+            do
+            {
+                Console.Write("Enter instruction:>");
+                input = Console.ReadLine().Trim().Split().ToList();
+
+                bool IsAnomaly = false;
+                if (!PartCounts.Contains(input.Count)) { IsAnomaly = true; }
+
+                if (!string.IsNullOrEmpty(input.First()))
+                {
+                    string CurrentInstruction = string.Empty;
+
+                    var MaybeInstruction = Instructions.Where(x => x == input.First().ToLower());
+                    if (MaybeInstruction.Any())
+                    {
+                        CurrentInstruction = MaybeInstruction.First();
+
+                        //ToDo
+
+                        if (CurrentInstruction == "register" && input.Count == 5)
+                        {
+
+                        }
+
+                        if (CurrentInstruction == "login" && input.Count == 5)
+                        {
+
+                        }
+
+                        if (CurrentInstruction == "change" && input.Count == 3)
+                        {
+
+                        }
+
+                        if (CurrentInstruction == "search" && input.Count == 3)
+                        {
+
+                        }
+
+                        if (CurrentInstruction == "changepassword" && input.Count == 5)
+                        {
+
+                        }
+
+                        if (CurrentInstruction == "logout" && input.Count == 1)
+                        {
+                            IsLogin = false;
+                            Console.WriteLine("You are logged out!");
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong instruction!");
+                        continue;
+                    }
+                }
+            }
+            while (input.First().ToLower() != "exit"); ///Feature: || input.ToLower() == "clear"
+
+            Environment.Exit(0);
         }
+
+        static List<string> Instructions = new List<string>
+        {
+            "register",
+            "login",
+            "change",
+            "search",
+            "changepassword",
+            "logout"
+        };
+
+        static List<string> Keys = new List<string>
+        {
+            "--username",
+            "--password",
+            "--status",
+            "--old",
+            "--new"
+        };
     }
 }
