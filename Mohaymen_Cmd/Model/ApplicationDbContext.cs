04b011771ba5f7928e8c08494737cfbbf326fff5
Mahdi-Agharezaei <79786754+Mahdi-Agharezaei@users.noreply.github.com>
2024-10-27 23:08:00 +0330

    Second commit (Add Model)

---
+++ Mohaymen_Cmd/Model/ApplicationDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mohaymen_Cmd.Model
+{
+    internal class ApplicationDbContext : DbContext
+    {
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.\\MSSQLLocalDB;Initial Catalog=Mohaymen_CmdDB;Integrated Security=True");
+        }
+
+        //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)//base("name=DefaultConnection")
+        //{
+
+        //}
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<User>().ToTable("myPersonTbl");
+
+        //    modelBuilder.Entity<User>().HasKey(x => x.ID);
+
+        //    modelBuilder.Entity<User>().Property(x => x.UserName)
+        //        .IsRequired()
+        //        .HasMaxLength(63);
+
+        //    modelBuilder.Entity<User>().Property(x => x.Password)
+        //        .IsRequired()
+        //        .HasMaxLength(63);
+
+        //    modelBuilder.Entity<User>().Property(x => x.Status)
+        //        .IsRequired()
+        //        .ValueGeneratedOnAdd();
+
+        //    modelBuilder.Entity<User>().Property(x => x.CreationDate)
+        //        .IsRequired()
+        //        .ValueGeneratedOnAdd();
+
+        //    modelBuilder.Entity<User>().Property(x => x.ModificationDate)
+        //        .IsRequired()
+        //        .ValueGeneratedOnAddOrUpdate();
+        //}
+    }
+}
---
+++ Mohaymen_Cmd/Model/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mohaymen_Cmd.Model
+{
+    internal class User
+    {
+        public required int ID { get; set; }
+        [MaxLength(63)]
+        public required string UserName { get; set; }
+        [MaxLength(63)]
+        public required string Password { get; set; }
+        public required bool Status { get; set; }
+        public required DateTime CreationDate { get; set; }
+        public required DateTime ModificationDate { get; set; }
+    }
+}
